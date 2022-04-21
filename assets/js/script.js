3cfc87927364b89069b513464e1324841f2a8452
Vidalatan <vidalatan@gmail.com>
2022-04-21 10:26:46 -0500

    Updated Style/Projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,54 @@
 // List of repositories to add to the page
 var repositories = {
-    // index: [link, image]
-    primary: ["https://github.com/Vidalatan/weather_dashboard", "https://raw.githubusercontent.com/Vidalatan/weather_dashboard/main/Assets/images/fullpage.png",false],
-    1: ["https://github.com/Vidalatan/scheduler", "https://raw.githubusercontent.com/Vidalatan/scheduler/main/Assets/images/FullPage.png",false],
-    2: ["https://github.com/Vidalatan/code_quiz", "https://raw.githubusercontent.com/Vidalatan/code_quiz/main/Assets/Images/demo.gif",false],
-    3: ["https://github.com/Vidalatan/password_generator", "https://raw.githubusercontent.com/Vidalatan/password_generator/main/readme_resource/full_site_w_pass.png",true],
-    4: ["https://github.com/Vidalatan/Mini-project", "https://raw.githubusercontent.com/Vidalatan/Mini-project/main/assets/MetaPets-Page.png",false],
-    5: ["https://github.com/Vidalatan/semantic-html", "https://raw.githubusercontent.com/Vidalatan/semantic-html/main/assets/images/horiseonpage.png", false]
+    // index: [name, link, image]
+    primary: ["Nerds Meet Nerds","https://nerds-meet-nerds.herokuapp.com/", "https://raw.githubusercontent.com/Nerds-Meet-Nerds/Nerds-Meet-Nerds/develop/public/assets/images/screenshots/landing-page.jpg",false],
+    1: ["Poke Shop","https://vidalatan.github.io/poke-shop/index.html", "https://raw.githubusercontent.com/Vidalatan/poke-shop/main/assets/images/poke-home-screenshot.png",false],
+    2: ["Tech Biz Blog Site","https://tech-biz-blog.herokuapp.com/", "https://raw.githubusercontent.com/Vidalatan/tech-biz-blog/main/assets/images/FinishedProductImage.png",false],
+    3: ["Weather Dashboard","https://vidalatan.github.io/weather_dashboard/", "https://raw.githubusercontent.com/Vidalatan/weather_dashboard/main/Assets/images/fullpage.png",false],
 };
 var primary_repo_size = 350;
 var secondary_repo_size = 250;
 
 
-// Function to grab the name of the repo from url and return it with better formatting
-function formatRepoName(url) {
-    let repo_name = url.substring(url.indexOf("tan/")+4);  // Grabs the repo name off of the end,
-    repo_name = repo_name.split(/[-,_]/g);                 // Split on "-" or "_" chars
+// Depreciated - Function to grab the name of the repo from url and return it with better formatting
+// function formatRepoName(url) {
+//     let repo_name = url.substring(url.indexOf("tan/")+4);  // Grabs the repo name off of the end,
+//     repo_name = repo_name.split(/[-,_]/g);                 // Split on "-" or "_" chars
     
-    // Take each word in the array and capitalize the first letter
-    for (let index in repo_name) {
-        repo_name[index] = repo_name[index].charAt(0).toUpperCase()+repo_name[index].substring(1);
-    }
-    repo_name = repo_name.join(" ");  // Join all the newly capitalized words together with whitespace
+//     // Take each word in the array and capitalize the first letter
+//     for (let index in repo_name) {
+//         repo_name[index] = repo_name[index].charAt(0).toUpperCase()+repo_name[index].substring(1);
+//     }
+//     repo_name = repo_name.join(" ");  // Join all the newly capitalized words together with whitespace
     
-    return repo_name
-}
+//     return repo_name
+// }
 
 $("#primary-project").append($("<div>").addClass("container d-flex justify-content-center")
     .append($("<a>").addClass(function() {
-        if (repositories.primary[2]) {
+        if (repositories.primary[3]) {
             return "project-link-alt primary-project"
         } else {
             return "project-link primary-project"
         }
     })
-        .attr("href", repositories.primary[0]).attr("target", "_blank")
-        .attr("style", "background-image: url("+repositories.primary[1]+"); background-position: center; background-size: auto "+primary_repo_size+"px; background-repeat: no-repeat; width: "+primary_repo_size+"px; height: "+primary_repo_size+"px;")
-            .append($("<p>").addClass("text-center").attr("style", "margin-top: "+primary_repo_size/2+"px;").text(formatRepoName(repositories.primary[0])))));
+        .attr("href", repositories.primary[1]).attr("target", "_blank")
+        .attr("style", "background-image: url("+repositories.primary[2]+"); background-position: center; background-size: auto "+primary_repo_size+"px; background-repeat: no-repeat; width: "+primary_repo_size+"px; height: "+primary_repo_size+"px;")
+            .append($("<p>").addClass("nameplate text-center").attr("style", "margin-top: "+primary_repo_size/2+"px;").text(repositories.primary[0]))));
 
 for (let index = 1; index < Object.keys(repositories).length; index++) {
     $("#secondary-projects").append($("<div>").addClass("col d-flex justify-content-center m-3")
         .append($("<a>").addClass(function() {
-            if (repositories[index][2]) {
+            if (repositories[index][3]) {
                 return "project-link pl-alt secondary-project"
             } else {
                 return "project-link secondary-project"
             }
         })
-            .attr("href", repositories[index][0]).attr("target", "_blank")
-            .attr("style", "background-image: url("+repositories[index][1]+"); background-position: center; background-size: auto "+secondary_repo_size+"px; background-repeat: no-repeat; width: "+secondary_repo_size+"px; height: "+secondary_repo_size+"px;")
-            .append($("<p>").addClass("text-center").attr("style", "margin-top: "+secondary_repo_size/2+"px;")  // Custom function that 
-            .text(formatRepoName(repositories[index][0])))))
+            .attr("href", repositories[index][1]).attr("target", "_blank")
+            .attr("style", "background-image: url("+repositories[index][2]+"); background-position: center; background-size: auto "+secondary_repo_size+"px; background-repeat: no-repeat; width: "+secondary_repo_size+"px; height: "+secondary_repo_size+"px;")
+            .append($("<p>").addClass("nameplate text-center").attr("style", "margin-top: "+secondary_repo_size/2+"px;")  // Custom function that 
+            .text(repositories[index][0]))))
 }
 
 $(".primary-project").parent().hover(eventIn => {
